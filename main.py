600b5e4baf396f2fa84b9cf4c4b3291be0aae129
Migara Bhanu <76873393+migarabhanu@users.noreply.github.com>
2021-09-09 19:32:46 +0530

    Added the converter

---
+++ main.py
@@ -0,0 +1,9 @@
+# Please consider this converter hasn't up-to-date.
+# Only updated in 6:40 PM of 9/9/2021.
+# CONSIDER 09/09/2021 DATE !!!
+
+rupee = 199.80
+
+ask_price = int(input(" Price in USD : "))
+answer = ask_price * rupee
+print(f" Price in LKR : {answer}")
